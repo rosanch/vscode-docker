b89f9936430a8a194761894d0af24a598ecc69e7
Julia Lieberman <juliaglieberman@gmail.com>
2018-07-23 16:29:51 -0700

    added delete possibility for input bar

--- commands/delete-registry.ts
+++ commands/delete-registry.ts
@@ -9,18 +9,24 @@ import { RegistryRootNode } from "../explorer/models/registryRootNode";
 import { ServiceClientCredentials } from 'ms-rest';
 import { RegistryNameStatus, RegistryListResult } from "azure-arm-containerregistry/lib/models";
 const teleCmdId: string = 'vscode-docker.deleteRegistry';
-import { AzureCredentialsManager } from '../utils/AzureCredentialsManager'
+import { AzureCredentialsManager } from '../utils/AzureCredentialsManager';
 import { AzureRegistryNode, AzureLoadingNode, AzureNotSignedInNode } from '../explorer/models/azureRegistryNodes';
 
-
+/**
+ * delete a registry and all it's associated nested items
+ * @param context : the AzureRegistryNode the user right clicked on to delete
+ */
 export async function deleteRegistry(context?: AzureRegistryNode) {
+    if (!context) {
+        deleteRegistryNoContext();
+        return;
+    }
     let opt: vscode.InputBoxOptions = {
         ignoreFocusOut: true,
         placeHolder: 'No',
         value: 'No',
         prompt: 'Are you sure you want to delete this registry and its associated images? Enter Y or N: '
     };
-    console.log(context);
     let answer = await vscode.window.showInputBox(opt);
     if (answer == 'N' || answer == 'n') { return };
 
@@ -59,3 +65,63 @@ export async function deleteRegistry(context?: AzureRegistryNode) {
 
 }
 
+/**
+ * this is called if the command is called through the input bar as opposed to on an AzureRegistryNode
+ */
+async function deleteRegistryNoContext() {
+
+    let azureAccount = await AzureCredentialsManager.getInstance().getAccount();
+    let registries = await AzureCredentialsManager.getInstance().getRegistries();
+    let reg: string[] = [];
+    for (let i = 0; i < registries.length; i++) {
+        reg.push(registries[i].name);
+    }
+    let desired = await vscode.window.showQuickPick(reg, { 'canPickMany': false, 'placeHolder': 'Choose a Registry to delete' });
+    if (desired === undefined) throw 'user Exit';
+    let registry = registries.find(reg => { return desired === reg.name });
+
+    let opt: vscode.InputBoxOptions = {
+        ignoreFocusOut: true,
+        placeHolder: 'No',
+        value: 'No',
+        prompt: 'Are you sure you want to delete this registry and its associated images? Enter Yes or No: '
+    };
+    //ensure user truly wants to delete registry
+    let answer = await vscode.window.showInputBox(opt);
+    if (answer !== 'Yes') return;
+
+    if (!azureAccount) {
+        return;
+    }
+
+    if (azureAccount.status === 'LoggedOut') {
+        return;
+    }
+    let resourceGroup: string;
+    let subscriptionId: string;
+    // grab the resource group and subscription ID to be passed in as parameters to beginDeleteMethod
+    for (let i = 0; i < registries.length; i++) {
+        if (registries[i].name === registry.name) {
+            resourceGroup = registries[i].id.slice(registries[i].id.search('resourceGroups/') + 'resourceGroups/'.length, registries[i].id.search('/providers/'));
+            subscriptionId = registries[i].id.slice('/subscriptions/'.length, registries[i].id.search('/resourceGroups/'));
+            break;
+        }
+    }
+
+    if (!resourceGroup || !subscriptionId) {
+        throw 'Something went wrong, this registry may no longer exist'
+    }
+
+    //get all the subscriptions to look through
+    const subs = AzureCredentialsManager.getInstance().getFilteredSubscriptionList();
+    //get the actual subscription object by using the id found on the registry id above
+    const subscription = subs.find(function (sub): boolean {
+        return sub.subscriptionId === subscriptionId;
+    });
+    const client = AzureCredentialsManager.getInstance().getContainerRegistryManagementClient(subscription);
+    await client.registries.beginDeleteMethod(resourceGroup, registry.name).then(function (response) {
+        console.log("Success!");
+    }, function (error) {
+        console.error("Failed!", error);
+    })
+}

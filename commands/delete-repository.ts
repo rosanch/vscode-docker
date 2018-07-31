bcd0ae28e837a879565cedeb0dcae09db2d4c2d0
Julia Lieberman <juliaglieberman@gmail.com>
2018-07-30 18:07:13 -0700

    merged functionalities so now it works more efficiently! gets correct credentials

--- commands/delete-repository.ts
+++ commands/delete-repository.ts
@@ -17,33 +17,9 @@ import { AzureRepositoryNode } from '../explorer/models/AzureRegistryNodes';
 import { Repository, getRepositories, getRepositoryInfo } from "../explorer/utils/dockerHubUtils";
 
 
-/**
- * delete a registry and all it's associated nested items
- * @param context : the AzureRegistryNode the user right clicked on to delete
- */
 export async function deleteRepository(context?: AzureRepositoryNode) {
-    if (!context) {
-        //deleteRepositoryNoContext();
-        vscode.window.showErrorMessage('You must right click on a valid repository to delete it');
-        return;
-    }
-    else {
-        deleteRepositoryContextAvailable(context);
-        return;
-    }
-}
 
-async function deleteRepositoryContextAvailable(context: AzureRepositoryNode) {
-    let opt: vscode.InputBoxOptions = {
-        ignoreFocusOut: true,
-        placeHolder: 'No',
-        value: 'No',
-        prompt: 'Are you sure you want to delete this repository and all of its associated images? Enter Yes or No: '
-    };
-    let answer = await vscode.window.showInputBox(opt);
-    if (answer !== 'Yes') { return };
-
-    let azureAccount = context.azureAccount;
+    let azureAccount = await AzureCredentialsManager.getInstance().getAccount();
     if (!azureAccount) {
         return;
     }
@@ -51,23 +27,67 @@ async function deleteRepositoryContextAvailable(context: AzureRepositoryNode) {
     if (azureAccount.status === 'LoggedOut') {
         return;
     }
-    let registry: Registry = context.registry;
-    let repoName = context.label;
-    let subscription: SubscriptionModels.Subscription = context.subscription;
-    let subscriptionid: string = context.subscription.subscriptionId;
-    let loginserver = context.registry.loginServer;
-    let resourceGroup = registry.id.slice(registry.id.search('resourceGroups/') + 'resourceGroups/'.length, registry.id.search('/providers/'));
-    let creds: { password: string, username: string } = await loginCredentials(subscription, registry);
+    let registry: Registry;
+    let subscription: SubscriptionModels.Subscription;
+    let repoName: string;
+    if (!context) {
+        let registries = await AzureCredentialsManager.getInstance().getRegistries();
+        let reg: string[] = [];
+        for (let i = 0; i < registries.length; i++) {
+            reg.push(registries[i].name);
+        }
+        let desired = await vscode.window.showQuickPick(reg, { 'canPickMany': false, 'placeHolder': 'Choose the Registry from which your desired repository exists' });
+        if (desired === undefined) return;
+        registry = registries.find(reg => { return desired === reg.name });
+        let subscriptionId = registry.id.slice('/subscriptions/'.length, registry.id.search('/resourceGroups/'));
+        const subs = AzureCredentialsManager.getInstance().getFilteredSubscriptionList();
+        //get the actual subscription object by using the id found on the registry id above
+        subscription = subs.find(function (sub): boolean {
+            return sub.subscriptionId === subscriptionId;
+        });
+        const myRepos: azureUtils.Repository[] = await azureUtils.getAzureRepositories(registry);
 
-    if (!resourceGroup || !subscriptionid) {
-        throw 'Something went wrong, could not find resource group and/or subscription id';
+
+        let rep: string[] = [];
+        for (let j = 0; j < myRepos.length; j++) {
+            rep.push(myRepos[j].name);
+        }
+        let desiredRepo = await vscode.window.showQuickPick(rep, { 'canPickMany': false, 'placeHolder': 'Choose the repository you want to delete' });
+        if (desiredRepo === undefined) return;
+        let repository = myRepos.find(rep => { return desiredRepo === rep.name });
+        repoName = repository.name;
     }
-    //  const client = AzureCredentialsManager.getInstance().getContainerRegistryManagementClient(context.subscription);
+    let opt: vscode.InputBoxOptions = {
+        ignoreFocusOut: true,
+        placeHolder: 'No',
+        value: 'No',
+        prompt: 'Are you sure you want to delete this repository and its associated images? Enter Yes or No: '
+    };
+    //ensure user truly wants to delete registry
+    let answer = await vscode.window.showInputBox(opt);
+    if (answer !== 'Yes') return;
+    let username: string;
+    let password: string;
+    if (context) {
+        // let creds = await loginCredentia5ls(context.subscription, context.registry, context);
+        // console.log('password from creds= ' + creds.password + ' username from creds= ' + creds.username);
+        username = context.userName;
+        password = context.password;
+        console.log('password from context= ' + password + ' username from context= ' + username);
+        repoName = context.label;
+        subscription = context.subscription;
+        registry = context.registry;
+    }
+    else {
+        let creds = await loginCredentials(subscription, registry);
+        username = creds.username;
+        password = creds.password;
+    }
     let path = `/v2/_acr/${repoName}/repository`;
-    let r = await request_data_from_registry('delete', loginserver, path, creds.username, creds.password);
-    console.log(r);
-
+    let r = await request_data_from_registry('delete', registry.loginServer, path, username, password);
 }
+
+
 
 async function request_data_from_registry(http_method: string, login_server: string, path: string, username: string, password: string) {
     let url: string = `https://${login_server}${path}`;
@@ -77,12 +97,16 @@ async function request_data_from_registry(http_method: string, login_server: str
         http_method: http_method,
         url: url
     }
+    let err = false;
     try {
         let response = await request.delete(opt);
     } catch (error) {
+        err = true;
         console.log(error);
     }
-
+    if (!err) {
+        vscode.window.showInformationMessage('Successfully deleted repository');
+    }
     //does this need params, json, and verify?
 }
 
@@ -93,7 +117,7 @@ function _get_authorization_header(username: string, password: string): string {
 
 
 //Implements new Service principal model for ACR container registries while maintaining old admin enabled use
-async function loginCredentials(subscription, registry): Promise<{ password: string, username: string }> {
+async function loginCredentials(subscription, registry, context?: AzureRepositoryNode): Promise<{ password: string, username: string }> {
     let username: string;
     let password: string;
     const client = AzureCredentialsManager.getInstance().getContainerRegistryManagementClient(subscription);
@@ -104,23 +128,9 @@ async function loginCredentials(subscription, registry): Promise<{ password: str
         password = creds.passwords[0].value;
         username = creds.username;
     } else {
-
-        let opt: vscode.InputBoxOptions = {
-            ignoreFocusOut: false,
-            prompt: 'Service Principal ID ?'
-        };
-
-        username = await vscode.window.showInputBox(opt);
-        if (username) throw ('No input from user received for Service Principal ID');
-
-        opt = {
-            ignoreFocusOut: false,
-            prompt: 'Service Principal Password ?'
-        };
-        password = await vscode.window.showInputBox(opt);
-
-        if (!password) throw ('No input from user received for Service Principal Password');
-
+        let creds = await azureUtils.getAccessCredentials(registry);
+        password = creds.accessToken;
+        username = '00000000-0000-0000-0000-000000000000';
     }
     return { password, username };
 }
@@ -131,189 +141,3 @@ function encode(str: string): string {
     let bufferLat1 = new Buffer(bufferB64.toString('base64'));
     return bufferLat1.toString('latin1');
 }
-
-/**
- * this is called if the command is called through the input bar as opposed to on an AzureRegistryNode
- *
- *
- *
- *     url = 'https://{}{}'.format(login_server, path)
-    headers = _get_authorization_header(username, password)
-    for i in range(0, retry_times):
-        errorMessage = None
-        try:
-
-            response = requests.request(
-                method=http_method,
-                url=url,
-                headers=headers,
-                params=params,
-                json=json_payload,
-                verify=(not should_disable_connection_verify())
-            )
- *
- *
-async function deleteRepositoryNoContext() {
-
-    let azureAccount = await AzureCredentialsManager.getInstance().getAccount();
-    let registries = await AzureCredentialsManager.getInstance().getRegistries();
-    let reg: string[] = [];
-    for (let i = 0; i < registries.length; i++) {
-        reg.push(registries[i].name);
-    }
-    let desired = await vscode.window.showQuickPick(reg, { 'canPickMany': false, 'placeHolder': 'Choose the Registry from which your desired repository exists' });
-    if (desired === undefined) return;
-    let registry = registries.find(reg => { return desired === reg.name });
-    let resourceGroup: string = registry.id.slice(registry.id.search('resourceGroups/') + 'resourceGroups/'.length, registry.id.search('/providers/'));
-    let subscriptionId = registry.id.slice('/subscriptions/'.length, registry.id.search('/resourceGroups/'));
-    const subs = AzureCredentialsManager.getInstance().getFilteredSubscriptionList();
-    //get the actual subscription object by using the id found on the registry id above
-    const subscription = subs.find(function (sub): boolean {
-        return sub.subscriptionId === subscriptionId;
-    });
-    const client = AzureCredentialsManager.getInstance().getContainerRegistryManagementClient(subscription);
-
-
-
-
-
-
-    const repos: Repository[] = [];
-    let node: AzureRepositoryNode;
-
-    const tenantId: string = subscription.tenantId;
-
-    const session: AzureSession = this._azureAccount.sessions.find((s, i, array) => s.tenantId.toLowerCase() === tenantId.toLowerCase());
-    const { accessToken, refreshToken } = await acquireToken(session);
-
-    if (accessToken && refreshToken) {
-        let refreshTokenARC;
-        let accessTokenARC;
-
-        await request.post('https://' + registry.name + '/oauth2/exchange', {
-            form: {
-                grant_type: 'access_token_refresh_token',
-                service: registry.name,
-                tenant: tenantId,
-                refresh_token: refreshToken,
-                access_token: accessToken
-            }
-        }, (err, httpResponse, body) => {
-            if (body.length > 0) {
-                refreshTokenARC = JSON.parse(body).refresh_token;
-            } else {
-                return [];
-            }
-        });
-
-        await request.post('https://' + registry.name + '/oauth2/token', {
-            form: {
-                grant_type: 'refresh_token',
-                service: registry.name,
-                scope: 'registry:catalog:*',
-                refresh_token: refreshTokenARC
-            }
-        }, (err, httpResponse, body) => {
-            if (body.length > 0) {
-                accessTokenARC = JSON.parse(body).access_token;
-            } else {
-                return [];
-            }
-        });
-        await request.get('https://' + registry.name + '/v2/_catalog', {
-            auth: {
-                bearer: accessTokenARC
-            }
-        }, (err, httpResponse, body) => {
-            if (body.length > 0) {
-                const repositories = JSON.parse(body).repositories;
-                for (let i = 0; i < repositories.length; i++) {
-                    node = new AzureRepositoryNode(repositories[i], "azureRepositoryNode");
-                    node.accessTokenARC = accessTokenARC;
-                    node.azureAccount = azureAccount;
-                    node.password = element.password;
-                    node.refreshTokenARC = refreshTokenARC;
-                    node.registry = registry;
-                    node.repository = element.label;
-                    node.subscription = element.subscription;
-                    node.userName = element.userName;
-                    repoNodes.push(node);
-                }
-            }
-        });
-    }
-    //Note these are ordered by default in alphabetical order
-    return repoNodes;
-}
-
-
-
-
-
-
-
-
-    let rep: string[] = [];
-    for (let j = 0; j < myRepos.length; j++) {
-        rep.push(myRepos[j].name);
-    }
-    let desiredRepo = await vscode.window.showQuickPick(rep, { 'canPickMany': false, 'placeHolder': 'Choose the repository you want to delete' });
-    if (desiredRepo === undefined) return;
-    let repository = myRepos.find(rep => { return desiredRepo === rep.name });
-    let temp = await dockerHub.getRepositoryInfo(repository);
-    console.log(temp);
-
-    let opt: vscode.InputBoxOptions = {
-        ignoreFocusOut: true,
-        placeHolder: 'No',
-        value: 'No',
-        prompt: 'Are you sure you want to delete this repository and its associated images? Enter Yes or No: '
-    };
-    //ensure user truly wants to delete registry
-    let answer = await vscode.window.showInputBox(opt);
-    if (answer !== 'Yes') return;
-
-    if (!azureAccount) {
-        return;
-    }
-
-    if (azureAccount.status === 'LoggedOut') {
-        return;
-    }
-
-    // grab the resource group and subscription ID to be passed in as parameters to beginDeleteMethod
-    for (let i = 0; i < registries.length; i++) {
-        if (registries[i].name === registry.name) {
-            resourceGroup = registries[i].id.slice(registries[i].id.search('resourceGroups/') + 'resourceGroups/'.length, registries[i].id.search('/providers/'));
-            subscriptionId = registries[i].id.slice('/subscriptions/'.length, registries[i].id.search('/resourceGroups/'));
-            break;
-        }
-    }
-
-    if (!resourceGroup || !subscriptionId) {
-        throw 'Something went wrong, this registry may no longer exist'
-    }
-
-    //get all the subscriptions to look through
-
-
-}
-
-
-async function acquireToken(session: AzureSession) {
-    return new Promise<{ accessToken: string; refreshToken: string; }>((resolve, reject) => {
-        const credentials: any = session.credentials;
-        const environment: any = session.environment;
-        credentials.context.acquireToken(environment.activeDirectoryResourceId, credentials.username, credentials.clientId, function (err: any, result: any) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({
-                    accessToken: result.accessToken,
-                    refreshToken: result.refreshToken
-                });
-            }
-        });
-    });
-}
-*/
--- explorer/utils/azureUtils.ts
+++ explorer/utils/azureUtils.ts
@@ -1,39 +1,35 @@
 import * as opn from 'opn';
 import { AzureRepositoryNode, AzureImageNode, AzureRegistryNode } from '../models/azureRegistryNodes';
-import { AzureSession } from '../../typings/azure-account.api';
+import { AzureAccount, AzureSession } from '../../typings/azure-account.api';
 import { Registry } from 'azure-arm-containerregistry/lib/models';
 import * as vscode from "vscode";
 import request = require('request-promise');
 import { AzureCredentialsManager } from '../../utils/AzureCredentialsManager';
+import { SubscriptionModels } from 'azure-arm-resource';
 
 
+export class Repository {
+    accessToken: string;
+    azureAccount: AzureAccount;
+    //password: string;
+    refreshToken: string;
+    registry: Registry;
+    name: string;
+    subscription: SubscriptionModels.Subscription;
+    //username: string;
+    resourceGroupName: string;
 
-export interface Repository {
-    namespace: string
-    name: string
-};
-
-export interface RepositoryInfo {
-    user: string
-    name: string
-    namespace: string
-    repository_type: string
-    status: number
-    description: string
-    is_private: boolean
-    is_automated: boolean
-    can_edit: boolean
-    star_count: number
-    pull_count: number
-    last_updated: string
-    build_on_cloud: any
-    has_starred: boolean
-    full_description: string
-    affiliation: string
-    permissions: {
-        read: boolean
-        write: boolean
-        admin: boolean
+    constructor(accessToken: string, AzureAccount: AzureAccount, refreshToken: string, registry: Registry, repository: string, subscription:
+        SubscriptionModels.Subscription, resourceGroupName: string) {
+        this.accessToken = accessToken;
+        this.azureAccount = AzureAccount;
+        // this.password=password;
+        this.refreshToken = refreshToken;
+        this.registry = registry;
+        this.name = repository;
+        this.subscription = subscription;
+        this.resourceGroupName = resourceGroupName;
+        // this.username=username;
     }
 }
 
@@ -52,58 +48,53 @@ export function browseAzurePortal(context?: AzureRegistryNode | AzureRepositoryN
 }
 
 
-/*
-export async function getAzureRepositories(Authorization: string, registry: Registry): Promise<Repository[]> {
 
-
-    let repos: Repository[];
-
-    let options = {
-        method: 'GET',
-        uri: `https://${registry.name}/v2/users/${Authorization}/repositories/`,
-        headers: {
-            Authorization: 'JWT ' + _token.token
-        },
-        json: true
+export async function getAzureRepositories(registry: Registry): Promise<Repository[]> {
+    const allRepos: Repository[] = [];
+    let repo: Repository;
+    let resourceGroup: string = registry.id.slice(registry.id.search('resourceGroups/') + 'resourceGroups/'.length, registry.id.search('/providers/'));
+    let subscriptionId = registry.id.slice('/subscriptions/'.length, registry.id.search('/resourceGroups/'));
+    const subs = AzureCredentialsManager.getInstance().getFilteredSubscriptionList();
+    //get the actual subscription object by using the id found on the registry id above
+    const subscription = subs.find(function (sub): boolean {
+        return sub.subscriptionId === subscriptionId;
+    });
+    let azureAccount: AzureAccount = AzureCredentialsManager.getInstance().getAccount();
+    if (!this._azureAccount) {
+        return [];
     }
 
-    try {
-        repos = await request(options);
-    } catch (error) {
-        console.log(error);
-        vscode.window.showErrorMessage('Docker: Unable to retrieve Repositories');
-    }
+    const { accessToken, refreshToken } = await getAccessCredentials(registry);
 
-    return repos;
+    if (accessToken && refreshToken) {
+
+        await request.get('https://' + registry.loginServer + '/v2/_catalog', {
+            auth: {
+                bearer: accessToken
+            }
+        }, (err, httpResponse, body) => {
+            if (body.length > 0) {
+                const repositories = JSON.parse(body).repositories;
+                for (let i = 0; i < repositories.length; i++) {
+                    repo = new Repository(accessToken, azureAccount, refreshToken, registry, repositories[i], subscription, resourceGroup);
+                    allRepos.push(repo);
+                }
+            }
+        });
+    }
+    //Note these are ordered by default in alphabetical order
+    return allRepos;
 }
 
-export async function getRepositoryInfo(repository: Repository): Promise<any> {
-
-    let res: any;
-
-    let options = {
-        method: 'GET',
-        uri: `https://hub.docker.com/v2/repositories/${repository.namespace}/${repository.name}/`,
-        headers: {
-            Authorization: 'JWT ' + _token.token
-        },
-        json: true
-    }
-
-    try {
-        res = await request(options);
-    } catch (error) {
-        console.log(error);
-        vscode.window.showErrorMessage('Docker: Unable to get Repository Details');
-    }
-
-    return res;
-}
-*/
-export async function getAccessCredentials(context: AzureRepositoryNode): Promise<{ refreshToken: any, accessToken: any }> {
-
-    let azureAccount = AzureCredentialsManager.getInstance().getAccount();
-    const tenantId: string = context.subscription.tenantId;
+export async function getAccessCredentials(registry: Registry): Promise<{ refreshToken: any, accessToken: any }> {
+    let subscriptionId = registry.id.slice('/subscriptions/'.length, registry.id.search('/resourceGroups/'));
+    const subs = AzureCredentialsManager.getInstance().getFilteredSubscriptionList();
+    //get the actual subscription object by using the id found on the registry id above
+    const subscription = subs.find(function (sub): boolean {
+        return sub.subscriptionId === subscriptionId;
+    });
+    const tenantId: string = subscription.tenantId;
+    let azureAccount: AzureAccount = AzureCredentialsManager.getInstance().getAccount()
     if (!this._azureAccount) {
         return;
     }
@@ -115,10 +106,10 @@ export async function getAccessCredentials(context: AzureRepositoryNode): Promis
         let refreshTokenARC;
         let accessTokenARC;
 
-        await request.post('https://' + context.registry.name + '/oauth2/exchange', {
+        await request.post('https://' + registry.loginServer + '/oauth2/exchange', {
             form: {
                 grant_type: 'access_token_refresh_token',
-                service: context.registry.name,
+                service: registry.loginServer,
                 tenant: tenantId,
                 refresh_token: refreshToken,
                 access_token: accessToken
@@ -131,10 +122,10 @@ export async function getAccessCredentials(context: AzureRepositoryNode): Promis
             }
         });
 
-        await request.post('https://' + context.registry.name + '/oauth2/token', {
+        await request.post('https://' + registry.loginServer + '/oauth2/token', {
             form: {
                 grant_type: 'refresh_token',
-                service: context.registry.name,
+                service: registry.loginServer,
                 scope: 'registry:catalog:*',
                 refresh_token: refreshTokenARC
             }
@@ -151,8 +142,6 @@ export async function getAccessCredentials(context: AzureRepositoryNode): Promis
     }
     return { refreshToken, accessToken }
 }
-
-
 
 
 async function acquireToken(session: AzureSession) {

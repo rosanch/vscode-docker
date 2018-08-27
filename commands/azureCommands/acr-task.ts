f17e5a254306d3a6c084fb5719f40c8fec9c8891
jvstokes <stokesjv@uw.edu>
2018-08-27 11:59:39 -0700

    adding params from cli

--- commands/azureCommands/acr-task.ts
+++ commands/azureCommands/acr-task.ts
@@ -66,8 +66,10 @@ async function createTask(subscription: SubscriptionModels.Subscription, resourc
     let client = AzureUtilityManager.getInstance().getContainerRegistryManagementClient(subscription);
     console.log("uhh");
     let taskCreateParameters: BuildTask = {
+        'type': "Microsoft.ContainerRegistry/registries/buildTasks",
         'location': registry.location,
         'alias': buildTaskAlias,
+        'name': taskName,
         'sourceRepository': {
             'repositoryUrl': gitURL,
             'sourceControlType': sourceControlType,
@@ -80,23 +82,29 @@ async function createTask(subscription: SubscriptionModels.Subscription, resourc
                 'expiresIn': 1313141
             }
         },
-        'platform': { 'osType': 'Linux' },
+        'platform': { "cpu": 2, 'osType': 'Linux' },
         'status': 'Enabled',
         'timeout': 3600
     }
 
-    let buildTask = await client.buildTasks.create(resourceGroupName, registry.name, taskName, taskCreateParameters);
+    try {
+        await client.buildTasks.create(resourceGroupName, registry.name, taskName, taskCreateParameters);
+    } catch (error) {
+        console.log(error);
+    }
     const type: string = 'image';
     // stepName is set equal to taskName to prepare for new library without step
     const stepName = taskName + 'StepName';
     let dockerStep: DockerBuildStep = {
-        'type': 'DockerBuildStep',
+        'baseImageTrigger': 'Runtime',
+        'baseImageDependencies': null,
         'branch': 'master',
         'imageNames': [imageName],
         'noCache': false,
         'dockerFilePath': 'Dockerfile',
         'buildArguments': [],
-        'baseImageTrigger': 'Runtime'
+        'isPushEnabled': true,
+        'type': 'Docker'
     }
     /*let stepCreateParameters: BuildStep = {
         'properties': {
